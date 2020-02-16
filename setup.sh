f45511b32b3e6a7874f9b82b991274c4a03fd7ff
Michael Masas <hot.delek@gmail.com>
2020-02-16 21:58:57 +0200

    added socat

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd;
-sudo apt install curl vim wget gcc docker.io -y;
+sudo apt install curl vim wget gcc docker.io socat -y;
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl;
 sudo chmod +x kubectl 
 sudo mv kubectl /usr/local/bin/;
